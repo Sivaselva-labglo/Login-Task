d7aa46569fc5fe79394453dbc185fef8b6963117
Sivaselva-labglo <sivaselva.labglo@gmail.com>
2022-10-19 13:48:44 +0530

    Logout feature  added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './loginPage';
 import UsersPage from './usersPage';
+import PrivateRouter from './PrivateRouter';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path='/' element={<LoginPage/>} />
-        <Route exact path='/:userspage' element={<UsersPage/>} />
-      </Routes>
-    </Router>
+    return (
+        <Router>
+            <Routes>
+                <Route exact path='/' element={<LoginPage />} />
+                <Route exact path='/:userspage' element={
+                    <PrivateRouter>
+                        <UsersPage />
+                    </PrivateRouter>
+                } />
+            </Routes>
+        </Router>
 
-  );
+    );
 }
 
 export default App;
